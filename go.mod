5bd7a31a1ef74c8e29372c28d9effbe9fbda4816
Ademking <ademkingnew@gmail.com>
2023-02-13 07:24:04 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ademking/domains
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func extract_domain(input string) string {
+	r, err := regexp.Compile(`([a-z0-9][a-z0-9\-]{0,61}[a-z0-9]\.)+[a-z0-9][a-z0-9\-]*[a-z0-9]`)
+	if err != nil {
+		return ""
+	}
+	if r.MatchString(input) {
+		return r.FindString(input)
+	}
+	return ""
+}
+
+func help() {
+	fmt.Println(`       __                      _           `)
+	fmt.Println(`  ____/ /___  ____ ___  ____ _(_)___  _____`)
+	fmt.Println(` / __  / __ \/ __ ` + "`" + `__ \/ __ ` + "`" + `/ / __ \/ ___/`)
+	fmt.Println(`/ /_/ / /_/ / / / / / / /_/ / / / / (__  ) `)
+	fmt.Println(`\__,_/\____/_/ /_/ /_/\__,_/_/_/ /_/____/  `)
+	fmt.Println(`                                           `)
+	fmt.Println("Usage: cat domains.txt | domains [-prefix string] [-suffix string]")
+	flag.PrintDefaults()
+	fmt.Println(`Examples:`)
+	fmt.Println(`  cat logs.txt | domains`)
+	fmt.Println(`  cat logs.txt | domains --prefix "https://" --suffix "/api"`)
+	fmt.Println(`  domains --input logs.txt`)
+	fmt.Println()
+	os.Exit(0)
+}
+
+func format_domain(prefix string, domain string, suffix string) string {
+	if prefix != "" {
+		domain = prefix + domain
+	}
+	if suffix != "" {
+		domain = domain + suffix
+	}
+	return domain
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	domains := make(map[string]bool)
+	prefixStr := flag.String("prefix", "", "Add a string at the beginning of the domain")
+	suffixStr := flag.String("suffix", "", "Add a string at the end of the domain")
+	filePath := flag.String("input", "", "Read from a file")
+	helpStr := flag.Bool("h", false, "Show this help")
+	flag.Parse()
+
+	if *helpStr {
+		help()
+	}
+
+	if *filePath == "" {
+		for scanner.Scan() {
+			input := scanner.Text()
+			input = strings.TrimSpace(input)
+			if input == "" {
+				continue
+			}
+			domain := extract_domain(input)
+			if domain == "" {
+				continue
+			}
+			domains[domain] = true
+		}
+
+		for domain := range domains {
+			domain = format_domain(*prefixStr, domain, *suffixStr)
+			os.Stdout.WriteString(domain + "\n")
+		}
+
+	} else {
+		file, err := os.Open(*filePath)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer file.Close()
+
+		// Read domains from file
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			input := scanner.Text()
+			input = strings.TrimSpace(input)
+			if input == "" {
+				continue
+			}
+			domain := extract_domain(input)
+			if domain == "" {
+				continue
+			}
+			domains[domain] = true
+		}
+
+		for domain := range domains {
+			domain = format_domain(*prefixStr, domain, *suffixStr)
+			os.Stdout.WriteString(domain + "\n")
+		}
+	}
+}
